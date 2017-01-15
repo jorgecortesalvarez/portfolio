fc7752a041d3119acfefa1a8a04b505e3cc064ca
CORTESALVAREZ\Jorge <jorgecortesalvarez@gmail.com>
2017-01-14 19:23:04 -0500

    Mockup MDN Finished!

---
+++ Mockup MDN/js/parallax.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('section[data-type="parallax_section"]').each(function(){
+  var $bgobj = $(this); // Variable para asignacion de objeto
+
+  $(window).scroll(function() {
+  $window = $(window);
+  var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+  var coords = '50% '+ yPos + 'px';
+  $bgobj.css({ backgroundPosition: coords });
+  });
+  });
+});
